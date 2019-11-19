3e806e9ec680b4f95d67254af33a6c467253df70
Lopes <gustavo.lopesv3@gmail.com>
2019-11-19 12:22:42 -0500

    deploy

--- LocaMais/settings.py
+++ LocaMais/settings.py
@@ -123,7 +123,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static')
+
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
