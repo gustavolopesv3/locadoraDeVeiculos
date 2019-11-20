f8afe625846cd30be5f44a8287e9766e0cc809b5
gustavolopesv3 <38574162+gustavolopesv3@users.noreply.github.com>
2019-11-19 19:43:42 -0500

    Update 0022_alguel_data_recebimento.py

--- locadora/migrations/0022_alguel_data_recebimento.py
+++ locadora/migrations/0022_alguel_data_recebimento.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='alguel',
             name='data_recebimento',
-            field=models.CharField(default=0.0008104822369309739, max_length=15),
+            field=models.CharField(default=1, max_length=15),
             preserve_default=False,
         ),
     ]
