4b6d4b8dbc3d67f26a70986b97dc7470affed5af
lopes <gustavo.lopesv3@gmail.com>
2019-12-05 14:06:42 -0500

    exit heroku

--- LocaMais/settings.py
+++ LocaMais/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-#import django_heroku
+import django_heroku
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -129,4 +129,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
